b06adf59979c7eb75c41cc406359a20af65bc08b
Mauricio Duarte <prussdev@gmail.com>
2020-06-01 19:05:00 -0300

    Add button signup and forgot password

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,32 +1,90 @@
-import React from 'react';
-import { Image, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Image,
+  StatusBar,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container } from './styles';
+import {
+  Container,
+  ForgotPasswordButton,
+  ForgotPasswordButtonText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const [keyboardOpen, setKeyboardOpen] = useState(false);
+
+  Keyboard.addListener('keyboardDidShow', () => {
+    setKeyboardOpen(true);
+  });
+
+  Keyboard.addListener('keyboardDidHide', () => {
+    setKeyboardOpen(false);
+  });
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
-      <Container>
-        <Image style={{ marginBottom: 20 }} source={logoImg} />
-
-        <Input name="email" icon="mail" placeholder="E-mail" />
-
-        <Input name="password" icon="lock" placeholder="Senha" />
-
-        <Button
-          onPress={() => {
-            console.log('Deu');
-          }}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={{ flex: 1 }}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
         >
-          Entrar
-        </Button>
-      </Container>
+          <Container>
+            <Image style={{ marginBottom: 20 }} source={logoImg} />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Deu');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPasswordButton
+              onPress={() => {
+                console.log('Deu');
+              }}
+            >
+              <ForgotPasswordButtonText>
+                Esqueci minha senha
+              </ForgotPasswordButtonText>
+            </ForgotPasswordButton>
+
+            {!keyboardOpen && (
+              <CreateAccountButton
+                onPress={() => {
+                  console.log('Deu');
+                }}
+              >
+                <Icon name="log-in" size={16} />
+                <CreateAccountButtonText>
+                  Criar uma conta
+                </CreateAccountButtonText>
+              </CreateAccountButton>
+            )}
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -11,3 +11,33 @@ export const Text = styled.Text`
   font-size: 16px;
   font-family: 'Roboto-Regular';
 `;
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  margin-top: 16px;
+`;
+
+export const ForgotPasswordButtonText = styled.Text`
+  font-size: 16px;
+  font-family: 'Roboto-Medium';
+  color: #67647e;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 16px 0;
+  background-color: #fff;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  font-size: 18px;
+  font-family: 'Roboto-Bold';
+  color: #67647e;
+  margin-left: 16px;
+`;
